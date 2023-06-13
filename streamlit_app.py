f40ee02248860f1b12b1baca251e8abffcbd0892
Valentin Vogt <vogtva@student.ethz.ch>
2023-06-13 12:02:47 +0200

    Init

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,35 +4,46 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+#importing required libraries
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+import streamlit as st
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+from io import StringIO 
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+#adding a file uploader
 
-    points_per_turn = total_points / num_turns
+file = st.file_uploader("Please choose a file")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+if file is not None:
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    #To read file as bytes:
+
+    bytes_data = file.getvalue()
+
+    st.write(bytes_data)
+
+
+
+    #To convert to a string based IO:
+
+    stringio = StringIO(file.getvalue().decode("utf-8"))
+
+    st.write(stringio)
+
+
+
+    #To read file as string:
+
+    string_data = stringio.read()
+
+    st.write(string_data)
+
+
+
+    #Can be used wherever a "file-like" object is accepted:
+
+    df= pd.read_csv(file)
+
+    st.write(df)
